5813fa607f0b0f2a352a3769b35f020cb1a1ee82
TrumpetDoot <andyyin06@gmail.com>
2024-08-03 21:28:46 -0400

    Update QuantityBtn.jsx

--- frontend/src/components/QuantityBtn.jsx
+++ frontend/src/components/QuantityBtn.jsx
@@ -13,9 +13,10 @@ const QuantityBtn = ({item, totalPrice, setTotalPrice}) => {
         if (count > 0) {
             setCount(count - 1);
             setTotalPrice(totalPrice-Number(item.price));
-            item.quantity +=1; // whata does this update
+            item.quantity +=1; 
         }
     }
+    
     return <>
         <div className='box'>
             <h2>{item.name}</h2>
@@ -27,7 +28,6 @@ const QuantityBtn = ({item, totalPrice, setTotalPrice}) => {
                 <p class="count-num">{count}</p>
                 <button onClick={increase} class="plus">+</button>
             </div>
-            {/* <p>Quantity: {item.quantity}</p> */}
         </div>
     </>
 };
